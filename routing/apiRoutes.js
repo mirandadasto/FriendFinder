e9bcb7cce02d8ef23ca711b56eb3ea49e129431d
Miranda D'Asto <merlinockipella@gmail.com>
2020-03-22 14:25:09 -0600

    Finished coding htmlRoutes.js, working on apiRoutes.js I need more logic in it, but I'm checking to see if it runs now.

---
+++ routing/apiRoutes.js
@@ -0,0 +1,20 @@
+const friends = require('../app/data/friends.js');
+
+function createRoutes(app)
+{
+       app.get('/api/friends', function (request, response) 
+    {
+        response.json(friends)
+    });
+
+    app.post('/api/friends', function (request, response) 
+    {
+        const surveyResults = request.body;
+     
+        response.end();
+    });
+}
+
+module.exports = {
+    createRoutes
+};
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -5,6 +5,18 @@ function createRoutes(app)
     const basePath = path.join(__dirname, '..');
 
     console.log(basePath);
+
+    app.get('/', (request, response) => 
+    {
+        const filePath = path.join(basePath, "public/home.html");
+        response.sendFile(filePath);
+    });
+
+    app.get("/survey", (request, response) =>
+    {
+        const filePath = path.join(basePath, "public/survey.html");
+        response.sendFile(filePath);
+    });
 }
 
  module.exports = {
